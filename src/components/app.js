50009ddda3a0c929d8f6163fb8547e2a7b2481d0
Jerson Janke <jerson.janke@senior.com.br>
2017-02-15 12:51:51 -0200

    Change index.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+//() => fat arrow function set up by es6, function(){}
 const App = () => (
   <div>
     HI
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app';
-ReactDOM.render(
-    <App />
-  , document.querySelector('.container'));
+import App, { AppDescription } from './components/app';
+
+console.log(AppDescription);
+
+ReactDOM.render( <App />, document.querySelector('.container') ); //select .container index.html
