c4007419575b7e81975dbe738925a23d64f677a1
Jerson Janke <jerson.janke@senior.com.br>
2017-02-16 13:14:05 -0200

    Create input

--- src/components/app.js
+++ src/components/app.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import Button from './button';
 
 export default class App extends Component {
-  
+
   constructor(propos) {
     super(propos)
     this.state = {
-      counter: 0,
+      textValue: '',
+      value: '',
     };
   }
+
+  textEntered = (event) => {    
+    this.setState({ value: event.target.value});
+  }
+
+changeText = () => {
+  this.setState({ textValue: this.state.value });
+}
 
   render() {
     return (
       <div>
-        {this.state.counter}
-        <Button name={'Add 1'} clicked={() => this.setState({ counter: this.state.counter + 1 }) } />
+        {this.state.textValue}
+        <input type="text" value={this.state.value} onChange={this.textEntered} />
+        <Button name={'Add 1'} clicked={this.changeText} />        
       </div>
     );
   }
